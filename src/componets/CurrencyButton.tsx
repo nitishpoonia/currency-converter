a756ef9ff8a540674a4da351fd5bf38eaacacf18
Nitish Poonia <npoonia39@gmail.com>
2023-12-31 15:04:25 +0530

    Currency converter completed

---
+++ src/componets/CurrencyButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type CurrencyButtonProps = PropsWithChildren<{
+  name: string;
+  flag: string;
+}>;
+const CurrencyButton = (props: CurrencyButtonProps): React.JSX.Element => {
+  return (
+    <View style={styles.buttonContainer}>
+      <Text style={styles.flag}>{props.flag}</Text>
+      <Text style={styles.country}>{props.name}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  flag: {
+    fontSize: 30,
+    color: '#ffffff',
+    marginBottom: 4,
+  },
+  country: {
+    fontSize: 14,
+  },
+});
+export default CurrencyButton;
